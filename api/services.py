d0d90edb961bb942724422aa7da5df53d7094720
Abas-dev <abasisaac22@gmail.com>
2024-08-07 04:15:03 +0100

    feat: added chutility functions to the services.py

--- api/services.py
+++ api/services.py
@@ -1,5 +1,14 @@
 from sqlalchemy.orm import Session
 from api.models import User
+from api.schema import UserCreate
 
 def check_user_exist(db: Session, email: str):
-    user = db.query(User).filter(User.email == email)
+    user = db.query(User).filter(User.email == email).first()
+    return user 
+
+def insert_new_user(db: Session, user: UserCreate):
+    new_user = User(email= user.email, password= user.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
